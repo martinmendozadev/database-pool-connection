94fa00528a6cda99aecc2ebaaa42d6313d47dc4f
Martin Gómez <martincgomezm@gmail.com>
2019-09-23 19:49:23 -0600

    Interfaz Areglada
    La interfaz del Index Fue arreglada.

--- js/util/editortexto.js
+++
@@ -1,7 +0,0 @@
-bkLib.onDomLoaded(function() {
-    nicEditors.editors.push(new nicEditor().panelInstance(document.getElementById('Descripcion')));
-});
-/*
-bkLib.onDomLoaded(function() {
-    nicEditors.editors.push(new nicEditor().panelInstance(document.getElementById('Experiencia')));
-});*/
