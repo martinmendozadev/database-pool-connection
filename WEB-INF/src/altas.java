0d3890a5d37006f98d663039c6f37a56f710908f
seguridad-web <mcgm@itz.com>
2019-09-24 18:03:36 -0500

    Modifique la COnexion a la BD
    Cambie el la conexion convecional por un POOL y comente el código de altas

--- WEB-INF/src/altas.java
+++ WEB-INF/src/altas.java
@@ -1,74 +1,78 @@
+//Iportamos nuestro paquetes a utilizar
 import java.io.*;
-import java.rmi.*;
+import java.rmi.ServerException;
 import java.sql.*;
-import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.sql.DataSource;
+import javax.annotation.Resource;
 
 public class altas extends HttpServlet {
 
+  //Variables globales para almacenar valores del formulario
   String motivoIng, nombreMas, sexo, especie;
+
+   //Establecemos el DataSource
+  @Resource(name = "jdbc/veterinaria")
+  private DataSource miPool;
 
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServerException, IOException {
 
+    //Establecemos nuestro out
     PrintWriter out = response.getWriter();
 
-    try {
-      response.setContentType("text/html");
-      HttpSession sesion = request.getSession(true);
+    //Variables de Conexión
+    Connection miConexion = null;
+    Statement miStatement = null;
 
+    try {
+      //Formato de salida
+      response.setContentType("text/html");
+
+      //Iniciamos a crear nuestro HTML
       out.println("<HTML><CENTER>");
 
-      // Se reciben los valores del formulario1
+      //Se reciben los valores del formulario1
       motivoIng = request.getParameter("motivoIn");
       nombreMas = request.getParameter("nombrem");
       sexo = request.getParameter("hm1");
       especie = request.getParameter("hm2");
 
-      // Se impirme en una pagina
+      //Se impirme en etiquetas H2 los datos introducidos en el formulario.
       out.println("<h2> Motivo Ingreso: " + motivoIng + "</h2>");
       out.println("<h2> Nombre Mascota: " + nombreMas + "</h2>");
       out.println("<h2> Sexo Mascota: " + sexo + "</h2>");
       out.println("<h2> Especie: " + especie + "</h2>");
 
+      //Cerramos nuestro documento HTML
       out.println("</CENTER></HTML>");
+
     } catch (Exception e) {
-      out.println("Ocurrio un error en main:" + e);
+      //En caso de algún error nuestro Catch se ejecuta
+      out.println("Ocurrio un error en main:\n" + e);
     }
 
     try {
+      /*Iniciamos la conexión a la BD*/
 
-      // paso 1 registro del drive
-      out.println("Inicio <br>");
-      Class.forName("com.mysql.jdbc.Driver");
-      out.println("Driver registrado <br>");
+      //Paso 1 Obtenemos conexión
+      miConexion = miPool.getConnection();
+      out.println("<br>Conexion a BD establecida");
 
-      // paso 2 obtenemos conexion
-      String url = "jdbc:mysql://localhost:3306/veterinaria";
+      //Paso 2 Construimos nuestro Query
+      String miSql = "insert into datos (motivoIng,nombreMas,sexo,especie) values('" + motivoIng + "','" + nombreMas+ "','" + sexo + "','" + especie +"')";
+      out.println("<br>" + miSql);
 
-      String usuario = "usuario";
-      String password = "123qwe";
+      //Paso 3 Establecemos el canal de comunicación
+      miStatement = miConexion.createStatement();
+      out.println("<br>Canal a BD establecido");
 
-      out.println("Conectado ..." + url);
-      Connection conexion = DriverManager.getConnection(url, usuario, password);
-      out.println("<br> Conexion lista");
-
-      // paso 3 contruccion del query
-      String query = "insert into datos (motivoIng,nombreMas,sexo,especie) values('" + motivoIng + "','" + nombreMas+ "','" + sexo + "','" + especie +"')";
-      out.println("<br>" + query);
-
-      // paso 4 canal de comunicacion
-      Statement st = conexion.createStatement();
-
-      // paso 5 ejecucion query
-      st.executeUpdate(query);
-      out.println("<br>Insertado correctamente");
-
-      // paso 6 Cerrar st y la conexcion
-      st.close();
-      conexion.close();
+      //Paso 4 Ejecución del query
+      miStatement.executeUpdate(miSql);
+      out.println("<br>Query ejecutado correctamente");
 
     } catch (Exception e) {
+      //En caso de error se ejecuta el catch
       out.println("<br>Error en la BD<br>" + e.getMessage());
     }
 
